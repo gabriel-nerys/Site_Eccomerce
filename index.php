1ffbc4284ecb2c46bbeef6fd0f3d2f00dde88634
gabriel-nerys <biel.nerys150@gmail.com>
2023-05-11 18:53:02 -0300

    cabeçalho

--- index.php
+++ index.php
@@ -4,15 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <title>TenisWord</title>
 </head>
 <body>
     
-    <?php
-        include 'nav.php'
-    ?>
+    <?php include 'nav.php' ?>
+    <?php include 'cabecalho.html' ?>
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-        <div class="container-fluid">
+      <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <img src="img/asset-15.png" alt="" width="32" height="32" class="d-inline-block align-text-top">
                 TenisWord
@@ -44,4 +44,4 @@
             
             </ul>
         </div>
-      </nav>
+</nav>
