45abc6e00621fe548c4ca2aa68f72b7bcad53bf1
gabriel-nerys <biel.nerys150@gmail.com>
2023-05-11 17:54:48 -0300

    include navbar php

---
+++ nav.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="img/asset-15.png" alt="" width="32" height="32" class="d-inline-block align-text-top">
+                TenisWord
+              </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="nav nav-tabs" style="--bs-scroll-height: 100px;">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>              
+              <li class="nav-item">
+                <a class="nav-link">Lançamentos</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Categorias
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Nike</a></li>
+                  <li><a class="dropdown-item" href="#">Adidas</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Mizuno</a></li>
+                </ul>
+              </li>              
+            </ul>
+            <form class="d-flex" role="search" style="margin-left: 15px; margin-top: 3px;">
+              <input class="form-control me-2" type="search" placeholder="Pesquisar..." aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </form>
+            
+          </div>
+            <ul class="nav nav-pills justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contato</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Login</a>
+                </li>
+                
+            
+            </ul>
+        </div>
+      </nav>
