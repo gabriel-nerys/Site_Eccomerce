0e4ef6e4663b31863fda2492293e7b55e866558a
gabriel-nerys <biel.nerys150@gmail.com>
2023-05-12 16:03:35 -0300

    criação do rodapé

--- index.php
+++ index.php
@@ -15,6 +15,36 @@
     <?php include 'nav.php' ?>
     <?php include 'cabecalho.html' ?>
 
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <img src="https://placehold.it/450x320" class="img-responsive" style="width: 100%;" alt="">
+                <div><h2>Nome do produto</h2></div>
+                <div><h4>R$500,00</h4></div>
+            </div>
+
+            <div class="col-sm-3">
+                <img src="https://placehold.it/450x320" class="img-responsive" style="width: 100%;" alt="">
+                <div><h2>Nome do produto</h2></div>
+                <div><h4>R$500,00</h4></div>
+            </div>
+
+            <div class="col-sm-3">
+                <img src="https://placehold.it/450x320" class="img-responsive" style="width: 100%;" alt="">
+                <div><h2>Nome do produto</h2></div>
+                <div><h4>R$500,00</h4></div>
+            </div>
+
+            <div class="col-sm-3">
+                <img src="https://placehold.it/450x320" class="img-responsive" style="width: 100%;" alt="">
+                <div><h2>Nome do produto</h2></div>
+                <div><h4>R$500,00</h4></div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php include 'rodape.html' ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
