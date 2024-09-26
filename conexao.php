053d4f2485f5de908a4b5e84eba21b386964502f
gabriel-nerys <biel.nerys150@gmail.com>
2024-09-26 18:42:17 -0300

    #

--- conexao.php
+++ conexao.php
@@ -5,4 +5,5 @@
     $banco = "db_site_eccomerce";
 
     $cn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+    //$cn = new PDO("mysql://root:gQtwapVctqZKOBgOqmrBjfvHCABeFwbP@:3306/railway");
 ?>
